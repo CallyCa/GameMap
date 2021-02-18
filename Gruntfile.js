e20aec6b480f43ebed21bf32684836deb6606268
callyca <blackyrite@hotmail.com>
2021-02-18 15:22:54 -0300

    Config gruntfile and styles.css

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,4 +7,22 @@ module.exports = (grunt) => {
     ].forEach((task) => {
         grunt.loadNpmTasks(task);
     });
+
+    // configure plugins
+
+    grunt.initConfig({
+        cafeMocha: {
+            all: {src: 'qa/tests-*.js', options: { ui: 'tdd'},}
+        },
+        jshint: {
+            qa: ['Gruntfile.js', 'src/qa/**/*.js'],
+        },
+        exec: {
+            linkchecker: { cmd: 'linkchecker http://localhost:3000'}
+        },
+    });
+
+    // register tasks
+
+    grunt.registerTask('default', ['cafemocha', 'jshint', 'exec']);
 }
